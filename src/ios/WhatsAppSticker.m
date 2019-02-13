14609e2f5b18ca6bd329d5797091d55864a8212d
Vinícius Lourenço <vinyygamerlol@gmail.com>
2019-02-13 11:39:45 -0200

    try another approach

--- src/ios/WhatsAppSticker.m
+++ src/ios/WhatsAppSticker.m
@@ -18,13 +18,48 @@
 - (void)sendToWhatsapp:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
+    // NSString* identifier = [ valueForKey:@"identifier" ];
+    // NSString* name = [[command.arguments objectAtIndex:0] valueForKey:@"name" ];
+    // NSString* publisher = [[command.arguments objectAtIndex:0] valueForKey:@"publisher" ];
+    // NSString* trayImage = [[command.arguments objectAtIndex:0] valueForKey:@"trayImage" ];
+    // NSArray* stickers = [[command.arguments objectAtIndex:0] valueForKey:@"stickers" ];
 
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
+    // StickerPackManager.queue.async {
+    //   NSMutableDictionary *dict = [[NSMutableDictionary alloc]initWithCapacity:10];
+
+
+
+    //   for sticker in json["stickers"] as! [String] {
+    //     var stickerDict: [String: Any] = [:]
+        
+    //     [myArray addObject: @"New item"];
+    //     stickerDict["image_data"] = sticker
+
+    //     stickersArray.append(stickerDict)
+    //   }
+      
+    //   json["stickers"] = stickersArray
+
+    //   let result = Interoperability.send(json: json)
+    //   DispatchQueue.main.async {
+    //       pluginResult = CDVPluginResult(
+    //         status: CDVCommandStatus_OK
+    //       )
+    //   }
+    // }
+    NSString* json = [command.arguments objectAtIndex:0];
+
+    [UIPasteboard generalPasteboard] setData:json forPasteboardType:@"net.whatsapp.third-party.sticker-pack"];
+
+    UIApplication *application = [UIApplication sharedApplication];
+    NSURL *URL = [NSURL URLWithString:@"whatsapp://stickerPack"];
+    [application openURL:URL options:@{} completionHandler:^(BOOL success) {
+        if (success) {
+            NSLog(@"Opened url");
+        }
+    }];
+
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:"Deu certo"];
 
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }

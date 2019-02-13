10ad02d92b79fdd73074de5e2e10dd6005f0b37a
Vinícius Lourenço <vinyygamerlol@gmail.com>
2019-02-13 10:21:44 -0200

    fix minor errors

--- www/WhatsAppSticker.js
+++ www/WhatsAppSticker.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-module.exports.sendToWhatsapp = function (arg0, success, error) {
+exports.sendToWhatsapp = function (arg0, success, error) {
     exec(success, error, 'WhatsAppSticker', 'sendToWhatsapp', [arg0]);
 };
